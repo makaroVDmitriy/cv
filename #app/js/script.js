e7715e91dbeec5301074e1c3c7362c77efc18057
makaroVDmitriy <makarovdmitriy0707@gmail.com>
2021-07-22 21:01:27 +0300

    added swup, name animation, added responsive fonts

--- #app/js/script.js
+++ #app/js/script.js
@@ -8,6 +8,13 @@ $(function () {
 	/* burger */
 
 
+	/* signature svg */
+	const paths = document.querySelectorAll("#signature path");
+
+	for (let i = 0; i < paths.length; i++) {
+		console.log(`length ${i + 1} is ${Math.ceil(paths[i].getTotalLength())}`)
+	}
+	/* signature svg */
 
 
 });
